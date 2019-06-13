8565dd8a683494c9124f382b0a78bdbed5e4673f
Banu <banu@Banunun-Coplugu.local>
2019-06-13 15:39:59 +0300

    new page section added

--- src/components/header.js
+++ src/components/header.js
@@ -16,4 +16,6 @@ const Header = ({ siteTitle }) => (
   </div>
 )
 
+
 export default Header
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import '../layouts/index.css'
 
 const IndexPage = () => (
   <div className='HeroGroup'>
+
+    <div className='ScrollOne'>
     <div className='ContainerLeft'>
   
       <h1>Jam along <br/> without improvisation.</h1>
@@ -32,7 +34,46 @@ const IndexPage = () => (
     <img src={require('../assets/turntable.svg')} />
 </div>
 
-  </div>
+</div>
+
+<div className='ScrollTwo'>
+
+
+<h1>How it's gonna work?</h1>
+
+
+<div className='DisplayContainer'>
+
+<div className='SideLeft'>
+  
+  <h3>I'm a Designer</h3>
+
+<p> But I must explain to you how all this mistaken idea of denouncing 
+  pleasure and praising pain was born and I will give you a complete account 
+  of the system, and expound the actual teachings of the great explorer of the truth, 
+  the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, 
+  because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter 
+  consequences that are extremely painful. </p>
+</div>
+
+<div className='SideRight'>
+  
+  <h3>I'm a Developer</h3>
+
+<p> But I must explain to you how all this mistaken idea of denouncing 
+  pleasure and praising pain was born and I will give you a complete account 
+  of the system, and expound the actual teachings of the great explorer of the truth, 
+  the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, 
+  because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter 
+  consequences that are extremely painful. </p>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
 )
 
 export default IndexPage
